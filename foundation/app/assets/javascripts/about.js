a251ec2afbba8a8aca573a46ce91bd78772a3f89
Olufunsho Olagundoye <Funsholag@gmail.com>
2017-11-28 03:53:00 -0500

    Created bio cards for board of directors, styled, and made changes to footer

--- foundation/app/assets/javascripts/about.js
+++ foundation/app/assets/javascripts/about.js
@@ -1,9 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-// $(document).ready(function() {
-// 		var divs = $('.single-card');
-// 		for (var i = 0; i < divs.length; i += 4) {
-// 				divs.slice(i, i + 4).wrapAll('<div class="row"></div>');
-// 		}
-// });
