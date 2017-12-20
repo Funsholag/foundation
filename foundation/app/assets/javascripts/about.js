2e5e40d3029de3b652b1bdbab6b785e5575b43bc
Olufunsho Olagundoye <Funsholag@gmail.com>
2017-12-20 17:37:32 -0500

    Made changes to cards on the board and officers page

--- foundation/app/assets/javascripts/about.js
+++ foundation/app/assets/javascripts/about.js
@@ -1,3 +1,36 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+// var timeout;
+// var $cards = $(".card");
+// var size = $cards.length - 1;
+//
+// function highlight(count){
+//   timeout = setTimeout(function(){
+//     $cards.removeClass("orange");
+//     $cards[count].classList.add("orange");
+//
+//     clearTimeout(timeout);
+//     if(size === count){
+//        highlight((count - size));
+//     }else{
+//       highlight(count + 1);
+//     }
+//
+//   }, 1000);
+// }
+
+
+// $cards.off("click").on( "click", function() {
+//   clearTimeout(timeout);
+// });
+//
+// highlight(0)
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('#myModal1').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
